23197bba8028112a2b419352875fe3bf67b7ef7c
sajal1302 <50777789+sajal1302@users.noreply.github.com>
2020-01-18 00:04:54 +0530

    18.01.2020

---
+++ algorithm solutions/the hurdle race.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hurdleRace function below.
+def hurdleRace(k, height):
+    z=max(height)
+    if k>z:
+        return 0
+    else:
+        return z-k
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
