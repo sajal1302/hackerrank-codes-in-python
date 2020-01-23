a857d0b950dfcbb79bb991857f6258466c587f29
sajal1302 <50777789+sajal1302@users.noreply.github.com>
2020-01-23 10:04:45 +0530

    23.01.2020

---
+++ algorithm solutions/fibonacci modified.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    for i in range(n-2):
+        t3=t1+(t2**2)
+        t1=t2
+        t2=t3
+    return t3
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
